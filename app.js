dda55a1eadb90b24da2bce1ca0aa9fe50618308a
UmeHania07 <umehania.shahid@gmail.com>
2024-09-02 16:00:52 +0500

    done

---
+++ app.js
@@ -0,0 +1,91 @@
+// var a = new Date();
+// a.setDate(30);
+// a.setMonth(11);
+// a.setFullYear(2001);
+// a.setHours(9);
+// a.setMinutes(30);
+// a.setSeconds(45);
+// console.log(a);
+// var a = new Date();
+// console.log("Date ==>",a.getDate());
+// console.log("Day ==>",a.getDay());
+// console.log("Month ==>",a.getMonth());
+// console.log("year ==>",a.getFullYear());
+// console.log("Hours ==>",a.getHours());
+// console.log("Seconds==>",a.getSeconds());
+// console.log("Milliseconds ==>",a.getMilliseconds());
+// console.log("Time ==>",a.getTime());
+// console.log(a);
+// var cleanestCities = ["chaina", "pakistan", "indonesia","america","southaferica","zambabay","japan"];
+// for (var i = 0; i <= 4; i++){
+//      var citytoCheck = prompt("ENTER YOUR COUNTRY");
+// }
+// if (citytoCheck === cleanestCities [0]){
+//      alert("its one of the cleanest cities");
+// }
+// else if (citytoCheck === cleanestCities[1]){
+//      alert ("its one of the cleanest cities");
+// }
+// else if (citytoCheck === cleanestCities[2]){
+//      alert ("its one of the cleanest cities");
+// }
+// else if (citytoCheck === cleanestCities[3]){
+//      alert ("its one of the cleanest cities");
+// }
+// else if (citytoCheck === cleanestCities[4]){
+//      alert ("its one of the cleanest cities");
+// }
+// else{
+//     alert("its not on the list");
+// }
+
+// function hello(fname ="known" , lname="person" ){
+//     document.write("hello" + " " + fname + " " +lname);
+
+//     }
+//     hello("hania" , "khan");
+
+    
+//       var day = 0;
+
+//       switch(day){
+//       case 0:
+//           document.write("<h1>Today is Monday");
+//           break;
+//       case 1:
+//           document.write("<h1>Today is Tuesday");
+//           break;
+//       case 2:
+//           document.write("<h1>Today is Wednesday");
+//           break;
+//       case 3:
+//           document.write("<h1>Today is Thursday");
+//           break;
+//       case 4:
+//           document.write("<h1>Today is Friday");
+//           break;
+//       case 5:
+//           document.write("<h1>Today is Saturday");
+//           break;
+//           case 6:
+//           document.write("<h1>Today is Sunday");
+//           break;
+//           default:(document.write('<h1>Enter Valid Day.'));
+//       }
+
+//       document.write('total subject - 6 <br> total marks - 600 <br> <br>')
+//       function result(urdu,englis,math,islamiat,physic,chemistry){
+//           var a = urdu+englis+math+islamiat+physic+chemistry
+//           return a;
+//       }
+//       var b = result(50,50,50,50,50,50);
+//       document.write("marks obtain = " +b+ "<br>");
+
+//       function per(tm){
+//           var c = tm/600*100;
+//           return c;
+//       }
+//       var d = per(b);
+//       document.write("percentage - "+ d + "%" + "<br>" + "<br>");
+//       document.write("Your grade = C");
+
